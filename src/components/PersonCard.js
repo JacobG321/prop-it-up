542b5c5bd0a634186a37f813ddaf9fca1384164b
JacobG321 <jacobgruber1998@gmail.com>
2022-08-05 17:25:10 -0400

    Add age updater

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props
+    const {firstName, lastName, hairColor, initialAge} = props;
+
+    const [age, setAge] = useState({initialAge});
+
+    const addAge = () => {
+        setAge({initialAge:age.initialAge + 1})
+    };
+
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {age.initialAge} </p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={ addAge }>Birthday Button for {firstName} {lastName}</button>
         </div>
     )
 }
